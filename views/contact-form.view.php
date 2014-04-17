e7d1c26a6c79e8c8b8f9be0a80e2d2c84e3aa39d
Jay Williams <jay@myd3.com>
2014-04-17 10:55:56 -0700

    Add phone number to plugin

--- views/contact-form.view.php
+++ views/contact-form.view.php
@@ -13,14 +13,14 @@
         <form role="form" id="frmCSCF" name="frmCSCF" method="post">
             <?php wp_nonce_field('cscf_contact','cscf_nonce'); ?>
             <input type="hidden" name="post-id" value="<?php echo $postID; ?>">
-            
+
             <?php if (isset($contact->Errors['recaptcha'])) { ?>
                 <div class="control-group form-group">
                     <p class="text-error"><?php echo $contact->Errors['recaptcha']; ?></p>
-                </div>            
+                </div>
             <?php } ?>
 
-            <!-- name --> 
+            <!-- name -->
             <div class="control-group form-group<?php if (isset($contact->Errors['name'])) echo ' error has-error'; ?>">
                 <label for="cscf_name"><?php _e('Name:','cleanandsimple');?></label>
                 <div class="<?php echo cscf_PluginSettings::InputIcons() ? "input-group" : ""; ?>">
@@ -30,17 +30,37 @@
                     <input class="form-control input-xlarge"
                        data-rule-required="true"
                        data-msg-required="<?php _e('Please give your name.','cleanandsimple');?>"
-                       type="text" id="cscf_name" name="cscf[name]" 
-                       value="<?php echo $contact->Name; ?>" 
+                       type="text" id="cscf_name" name="cscf[name]"
+                       value="<?php echo $contact->Name; ?>"
                        placeholder="<?php _e('Your Name','cleanandsimple');?>"
                     />
                 </div>
                 <span for="cscf_name" class="help-inline help-block error" style="display:<?php echo isset($contact->Errors['name']) ? 'block' : 'none'; ?>;">
                     <?php if (isset($contact->Errors['name'])) echo $contact->Errors['name']; ?>
                 </span>
-            </div>  
+            </div>
 
-            
+            <!-- phone -->
+            <div class="control-group form-group<?php if (isset($contact->Errors['phone'])) echo ' error has-error'; ?>">
+                <label for="cscf_phone"><?php _e('Phone:','cleanandsimple');?></label>
+                <div class="<?php echo cscf_PluginSettings::InputIcons() ? "input-group" : ""; ?>">
+                    <?php if ( cscf_PluginSettings::InputIcons() == true ) { ?>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                    <?php } ?>
+                    <input class="form-control input-xlarge"
+                       data-rule-required="true"
+                       data-msg-required="<?php _e('Please give your phone number.','cleanandsimple');?>"
+                       type="text" id="cscf_phone" name="cscf[phone]"
+                       value="<?php echo $contact->Phone; ?>"
+                       placeholder="<?php _e('Your Phone','cleanandsimple');?>"
+                    />
+                </div>
+                <span for="cscf_phone" class="help-inline help-block error" style="display:<?php echo isset($contact->Errors['phone']) ? 'block' : 'none'; ?>;">
+                    <?php if (isset($contact->Errors['phone'])) echo $contact->Errors['phone']; ?>
+                </span>
+            </div>
+
+
             <!--email address-->
             <div class="control-group form-group<?php if (isset($contact->Errors['email'])) echo ' error has-error'; ?>">
                 <label for="cscf_email"><?php _e('Email Address:','cleanandsimple');?></label>
@@ -53,8 +73,8 @@
                         data-rule-email="true"
                         data-msg-required="<?php _e('Please give your email address.','cleanandsimple');?>"
                         data-msg-email="<?php _e('Please enter a valid email address.','cleanandsimple');?>"
-                        type="email" id="cscf_email" name="cscf[email]" 
-                        value="<?php echo $contact->Email; ?>" 
+                        type="email" id="cscf_email" name="cscf[email]"
+                        value="<?php echo $contact->Email; ?>"
                         placeholder="<?php _e('Your Email Address','cleanandsimple');?>"
                     />
                 </div>
@@ -78,15 +98,15 @@
                         data-msg-required="<?php _e('Please enter the same email address again.','cleanandsimple');?>"
                         data-msg-email="<?php _e('Please enter a valid email address.','cleanandsimple');?>"
                         data-msg-equalTo="<?php _e('Please enter the same email address again.','cleanandsimple');?>"
-                        type="email" id="cscf_confirm-email" name="cscf[confirm-email]" 
-                        value="<?php echo $contact->ConfirmEmail; ?>" 
+                        type="email" id="cscf_confirm-email" name="cscf[confirm-email]"
+                        value="<?php echo $contact->ConfirmEmail; ?>"
                         placeholder="<?php _e('Confirm Your Email Address','cleanandsimple');?>"
                     />
                 </div>
                 <span for="cscf_confirm-email" class="help-inline help-block error" style="display:<?php echo isset($contact->Errors['confirm-email']) ? 'block' : 'none'; ?>;">
                     <?php if (isset($contact->Errors['confirm-email'])) echo $contact->Errors['confirm-email']; ?>
                 </span>
-            </div> 
+            </div>
             <?php } ?>
 
 
@@ -100,7 +120,7 @@
                     <textarea class="form-control input-xlarge"
                         data-rule-required="true"
                         data-msg-required="<?php _e('Please give a message.','cleanandsimple');?>"
-                        id="cscf_message" name="cscf[message]" rows="10" 
+                        id="cscf_message" name="cscf[message]" rows="10"
                         placeholder="<?php _e('Your Message','cleanandsimple');?>"><?php echo $contact->Message; ?></textarea>
                 </div>
                 <span for="cscf_message" class="help-inline help-block error" style="display:<?php echo isset($contact->Errors['message']) ? 'block' : 'none'; ?>;">
@@ -114,15 +134,15 @@
                  var RecaptchaOptions = {
                     theme : '<?php echo cscf_PluginSettings::Theme(); ?>'
                  };
-                 </script>            
-                <div class="control-group form-group<?php 
+                 </script>
+                <div class="control-group form-group<?php
                     if (isset($contact->Errors['recaptcha'])) echo ' error'; ?>">
                         <div id="recaptcha_div">
                             <?php echo cscf_recaptcha_get_html($contact->RecaptchaPublicKey,null,isset($_SERVER['HTTPS'])); ?>
-                        <div for="cscf_recaptcha" class="help-block has-error error"><?php if (isset($contact->Errors['recaptcha'])) echo $contact->Errors['recaptcha']; ?></div> 
-                     </div>	
-                </div>	
-            <?php } ?>     
+                        <div for="cscf_recaptcha" class="help-block has-error error"><?php if (isset($contact->Errors['recaptcha'])) echo $contact->Errors['recaptcha']; ?></div>
+                     </div>
+                </div>
+            <?php } ?>
                 <input type="submit" class="btn btn-default" value="<?php _e('Send Message','cleanandsimple');?>"/>
         </form>
     </div>
